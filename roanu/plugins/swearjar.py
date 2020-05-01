26a8c47aa3396a622e0f5c37a12df2c4990bbe96
eyaadh <eyaadh@gmail.com>
2020-05-02 01:50:00 +0500

    added error exception for profanity check

--- roanu/plugins/swearjar.py
+++ roanu/plugins/swearjar.py
@@ -1,4 +1,5 @@
 import time
+import logging
 from better_profanity import profanity
 from roanu.utils.common import RoanuCommon
 from pyrogram import Client, Message, Filters, Emoji, ChatPermissions
@@ -15,26 +16,29 @@ async def swear_jar_resp(c: Client, m: Message):
 
     if m.from_user.id not in chat_admins_list and not m.service:
         mention = f"<a href='tg://user?id={m.from_user.id}'>{m.from_user.first_name}</a>"
-        if profanity.contains_profanity(m.text.encode('utf-8')):
-            swear_jar_counter.append(m.from_user.id)
-            swear_count = swear_jar_counter.count(m.from_user.id)
-            if 3 <= swear_count < 6:
-                owe_value = swear_count * 10
-                await m.reply(
-                    text=f"Hey {mention}! I guess you need to consider that you are at <b>a public chat</b> i.e. "
-                         f"talk with some respect! \nYou owe {Emoji.MONEY_WITH_WINGS} <i>${owe_value}</i> "
-                         f"{Emoji.MONEY_WITH_WINGS} to Swearjar now. \n<i>Once you reach <b>$60</b> you will be "
-                         f"restricted for a day from sending messages within this chat.</i>",
-                    parse_mode="html"
-                )
-            elif swear_count == 6:
-                swear_jar_counter = [x for x in swear_jar_counter if x != m.from_user.id]
-                await c.restrict_chat_member(chat_id=m.chat.id,
-                                             user_id=m.from_user.id,
-                                             permissions= ChatPermissions(
-                                                 can_send_messages=False),
-                                             until_date=int(time.time()+86400))
-                await m.reply_text(
-                    text=f"{Emoji.EXPLODING_HEAD} Ok that is enough {mention}! Now be restricted for "
-                         f"the next {Emoji.ALARM_CLOCK} 24HRS."
-                )
+        try:
+            if profanity.contains_profanity(m.text):
+                swear_jar_counter.append(m.from_user.id)
+                swear_count = swear_jar_counter.count(m.from_user.id)
+                if 3 <= swear_count < 6:
+                    owe_value = swear_count * 10
+                    await m.reply(
+                        text=f"Hey {mention}! I guess you need to consider that you are at <b>a public chat</b> i.e. "
+                             f"talk with some respect! \nYou owe {Emoji.MONEY_WITH_WINGS} <i>${owe_value}</i> "
+                             f"{Emoji.MONEY_WITH_WINGS} to Swearjar now. \n<i>Once you reach <b>$60</b> you will be "
+                             f"restricted for a day from sending messages within this chat.</i>",
+                        parse_mode="html"
+                    )
+                elif swear_count == 6:
+                    swear_jar_counter = [x for x in swear_jar_counter if x != m.from_user.id]
+                    await c.restrict_chat_member(chat_id=m.chat.id,
+                                                 user_id=m.from_user.id,
+                                                 permissions= ChatPermissions(
+                                                     can_send_messages=False),
+                                                 until_date=int(time.time()+86400))
+                    await m.reply_text(
+                        text=f"{Emoji.EXPLODING_HEAD} Ok that is enough {mention}! Now be restricted for "
+                             f"the next {Emoji.ALARM_CLOCK} 24HRS."
+                    )
+        except Exception as e:
+            logging.error(str(e))
